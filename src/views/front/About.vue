9bb385aea8746ade8a253cbf30a9b71abf3b1f1a
Carol Yang <carolyangya@gmail.com>
2020-10-30 18:25:52 +0800

    修改關於我頁面響應式版型

--- src/views/front/About.vue
+++ src/views/front/About.vue
@@ -5,22 +5,28 @@
     </div>
 
     <div class="brand">
-      <div class="brand-about" data-aos="zoom-in" data-aos-duration="1000">
+      <div class="brand-info" data-aos="zoom-in" data-aos-duration="1000">
         <h3 class="brand-title">關於 Adagio</h3>
         <p class="brand-text">
           Adagio 代表音樂中的慢板，在義大利文裡是緩慢而從容的意思，在這快節奏的是世界裡，
           適時地慢下來，留一點時間給自己，才能細細品味的生活。
         </p>
       </div>
-      <div class="brand-idea">
-        <div class="idea-content" data-aos="zoom-in" data-aos-duration="1000">
-          <h3 class="brand-title">經營理念</h3>
-          <p class="brand-text">
-            Adagio 堅持以「專業、誠信、服務、品質」的經營理念，我們用最真誠的態度
-            ，打造美味的佳餚，讓您來到 Adagio 都能有賓至如歸的感受。
-          </p>
-        </div>
-        <div class="idea-img"></div>
+      <div class="brand-slider">
+        <div class="brand-img"></div>
+      </div>
+    </div>
+
+    <div class="brand brand-idea">
+      <div class="brand-info" data-aos="zoom-in" data-aos-duration="1000">
+        <h3 class="brand-title">經營理念</h3>
+        <p class="brand-text">
+          Adagio 堅持以「專業、誠信、服務、品質」的經營理念，我們用最真誠的態度
+          ，打造美味的佳餚，讓您來到 Adagio 都能有賓至如歸的感受。
+        </p>
+      </div>
+      <div class="brand-slider">
+        <div class="brand-img"></div>
       </div>
     </div>
   </div>
@@ -44,13 +50,32 @@ export default {
 }
 
 .brand {
-  background: $light;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  @include desktop-s {
+    flex-direction: row;
+  }
+}
+
+@include desktop-s {
+  .brand-idea {
+    flex-direction: row-reverse;
+  }
+}
+
+.brand-info {
+  width: 100%;
+  padding: 3rem 0;
   text-align: center;
+  @include desktop-s {
+    width: 50%;
+  }
 }
 
 .brand-title {
   display: inline-block;
-  margin-bottom: 1.5rem;
+  margin-bottom: 2rem;
   padding-bottom: 0.5rem;
   border-bottom: 2px solid $dark;
   font-size: 1.75rem;
@@ -63,41 +88,19 @@ export default {
   letter-spacing: 0.25rem;
 }
 
-.brand-about {
-  padding: 5rem 0;
-  background: linear-gradient(90deg, $light 50%, $white 50%);
-  @include pad {
-    padding: 7rem 0;
-  }
-}
-
-.brand-idea {
-  display: flex;
-  flex-direction: column;
-  background: linear-gradient(90deg, $white 50%, $light 50%);
-  @include pad {
-    flex-direction: row;
-  }
-}
-
-.idea-content {
-  width: 100%;
-  margin: 0 auto;
-  padding: 5rem 0;
-  @include pad {
-    width: 50%;
-    padding: 7rem 0;
-  }
-}
-
-.idea-img {
+.brand-slider {
   flex: 1;
-  min-height: 20rem;
-  margin: 0;
+  width: 100%;
+  padding: 1.5rem;
+  background-color: $light;
+  @include desktop-s {
+    padding: 2rem;
+  }
+}
+
+.brand-img {
+  min-height: 24rem;
   background: url('../../assets/images/about.jpg') center center no-repeat;
   background-size: cover;
-  @include pad {
-    margin: 2rem;
-  }
 }
 </style>
